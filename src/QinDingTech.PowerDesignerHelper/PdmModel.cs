f1f0171e73196161c893c8cc44789b2813f29cd7
1-2-3 <zahuifan@163.com>
2017-03-01 14:50:12 +0800

    add PowerDesignerHelper

---
+++ src/QinDingTech.PowerDesignerHelper/PdmModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace QinDingTech.PowerDesignerHelper
+{
+	public class PdmModel
+	{
+		public PdmModel()
+		{
+			Tables = new List<TableInfo>();
+			Views = new List<ViewInfo>();
+            Procedures = new List<ProcedureInfo>();
+        }
+
+		/// <summary>
+		/// 表集合
+		/// </summary>
+		public IList<TableInfo> Tables { get; private set; }
+
+		/// <summary>
+		/// 视图集合
+		/// </summary>
+		public IList<ViewInfo> Views { get; private set; }
+
+        /// <summary>
+        /// 存储过程集合
+        /// </summary>
+        public IList<ProcedureInfo> Procedures { get; private set; }
+	}
+}
---
+++ src/QinDingTech.PowerDesignerHelper/ProcedureInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QinDingTech.PowerDesignerHelper
+{
+    /// <summary>
+    /// 存储过程信息
+    /// </summary>
+    public class ProcedureInfo
+    {
+        /// <summary>
+		/// 存储过程名
+		/// </summary>
+		public string Name { get; set; }
+
+        /// <summary>
+        /// 存储过程代码=>数据库中的存储过程名
+        /// </summary>
+        public string Code { get; set; }
+
+        /// <summary>
+        /// 存储过程定义=>数据库中的存储过程定义代码
+        /// </summary>
+        public string Text { get; set; }
+    }
+}
